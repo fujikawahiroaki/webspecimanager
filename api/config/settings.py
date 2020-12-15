b596ff6b0a1206dc0403831de96dd116a0755986
fujikawahiroaki <fhir0aki3@gmail.com>
2020-12-15 10:44:00 +0900

    ページネーション対応

--- api/config/settings.py
+++ api/config/settings.py
@@ -178,6 +178,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_auth0.authentication.Auth0JSONWebTokenAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
 }
 
 certificate_dir = os.path.join(BASE_DIR, "rsa_certificates/certificate.pem")
