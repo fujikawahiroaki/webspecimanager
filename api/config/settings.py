2f14a5117f7e81986a8a0acf8b127d97a769b22c
fujikawahiroaki <fhir0aki3@gmail.com>
2020-12-15 10:58:33 +0900

    CoreAPIドキュメント有効化

--- api/config/settings.py
+++ api/config/settings.py
@@ -180,6 +180,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
 
 certificate_dir = os.path.join(BASE_DIR, "rsa_certificates/certificate.pem")
--- api/config/urls.py
+++ api/config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls import include
 from django.conf.urls.static import static
+from rest_framework.documentation import include_docs_urls
 # from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
@@ -19,3 +20,7 @@ urlpatterns = [
 # 開発中のメディアファイル配信設定
 urlpatterns += static(settings.MEDIA_URL,
                       document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns += [path('docs/', include_docs_urls(title='APIドキュメント'))]
