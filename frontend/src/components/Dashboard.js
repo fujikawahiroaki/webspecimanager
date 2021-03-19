ca067337e7172ba5d449eb9966cb75ce377acfca
fujikawahiroaki <fhir0aki3@gmail.com>
2021-03-20 05:36:07 +0900

    ダッシュボードに注意事項を追記

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -8,5 +8,7 @@ export default () => (
         <CardHeader title="WebSpecimanager" />
         <CardContent>ようこそ</CardContent>
         <CardContent>必ず<a href="https://www.webspecimanager.net/manual/" target="_blank" rel="noopener">マニュアル</a>をお読みになってからご利用ください</CardContent>
+        <CardContent>現時点ではSafariブラウザではラベルPDF生成機能がご利用頂けません。またInternet Explorerおよび旧型Edgeはサポートしておりません。Google Chrome、FireFox、Edge(新型)のいずれかからご利用ください。</CardContent>
+        <CardContent>現時点ではモバイル版では表示が崩れます。お手数ですが、当面はスマホからのご利用の際もPC版で表示するようお願いいたします。</CardContent>
     </Card>
 );
