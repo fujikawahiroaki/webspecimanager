aa394283b725e86c89a192c5c631b5a4e9eca350
fujikawahiroaki <fhir0aki3@gmail.com>
2021-03-20 10:21:00 +0900

    メンテナンス通知

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -7,6 +7,7 @@ export default () => (
     <Card>
         <CardHeader title="WebSpecimanager" />
         <CardContent>ようこそ</CardContent>
+        <CardContent>メンテナンスのために3月21日早朝4時〜6時の間、システムを一時停止いたします。ご迷惑をおかけしますが、本サービスの性能向上のためご協力お願いいたします。</CardContent>
         <CardContent>必ず<a href="https://www.webspecimanager.net/manual/" target="_blank" rel="noopener">マニュアル</a>をお読みになってからご利用ください</CardContent>
         <CardContent>現時点ではSafariブラウザではラベルPDF生成機能がご利用頂けません。またInternet Explorerおよび旧型Edgeはサポートしておりません。Google Chrome、FireFox、Edge(新型)のいずれかからご利用ください。</CardContent>
         <CardContent>現時点ではモバイル版では表示が崩れます。お手数ですが、当面はスマホからのご利用の際もPC版で表示するようお願いいたします。</CardContent>
