96509929f9ad9b4115ea113ec48149555951e7c8
fujikawahiroaki <fhir0aki3@gmail.com>
2021-03-21 05:46:39 +0900

    メンテ告知削除

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -7,9 +7,8 @@ export default () => (
     <Card>
         <CardHeader title="WebSpecimanager" />
         <CardContent>ようこそ</CardContent>
-        <CardContent>メンテナンスのために3月21日早朝4時〜6時の間、システムを一時停止いたします。ご迷惑をおかけしますが、本サービスの性能向上のためご協力お願いいたします。</CardContent>
         <CardContent>必ず<a href="https://www.webspecimanager.net/manual/" target="_blank" rel="noopener">マニュアル</a>をお読みになってからご利用ください</CardContent>
-        <CardContent>標本データに対する「これをベースに作成」ボタンが正常に機能しないバグが発見されたため、いったんこの機能をオフにしました。ご不便をおかけしますが、修正が完了するまでしばらくお待ちください。</CardContent>
+        <CardContent>標本データに対する「これをベースに作成」ボタンが正常に機能しないバグが発見されたため、標本データのみいったん「これをベースに作成」ボタンを無くしました。ご不便をおかけしますが、修正が完了するまでしばらくお待ちください。</CardContent>
         <CardContent>現時点ではSafariブラウザではラベルPDF生成機能がご利用頂けません。またInternet Explorerおよび旧型Edgeはサポートしておりません。Google Chrome、FireFox、Edge(新型)のいずれかからご利用ください。</CardContent>
         <CardContent>現時点ではモバイル版では表示が崩れます。お手数ですが、当面はスマホからのご利用の際もPC版で表示するようお願いいたします。</CardContent>
     </Card>
