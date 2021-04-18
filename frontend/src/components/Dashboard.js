c578b5ece1fe18b8a544c0ab69f9893e4ee61c45
fujikawahiroaki <fhir0aki3@gmail.com>
2021-04-18 09:39:47 +0900

    ダッシュボードお知らせ更新

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -8,7 +8,9 @@ export default () => (
         <CardHeader title="WebSpecimanager" />
         <CardContent>ようこそ</CardContent>
         <CardContent>必ず<a href="https://www.webspecimanager.net/manual/" target="_blank" rel="noopener">マニュアル</a>をお読みになってからご利用ください</CardContent>
-        <CardContent>標本データに対する「これをベースに作成」ボタンが正常に機能しないバグが発見されたため、標本データのみいったん「これをベースに作成」ボタンを無くしました。ご不便をおかけしますが、修正が完了するまでしばらくお待ちください。</CardContent>
+        <CardContent>標本データに対する「これをベースに作成」ボタンが正常に機能しないバグを修正しました。</CardContent>
+        <CardContent>標本データ画像を投稿できないバグを修正しました。</CardContent>
+        <CardContent>標本IDを直接入力できないバグを修正しました。</CardContent>
         <CardContent>現時点ではSafariブラウザではラベルPDF生成機能がご利用頂けません。またInternet Explorerおよび旧型Edgeはサポートしておりません。Google Chrome、FireFox、Edge(新型)のいずれかからご利用ください。</CardContent>
         <CardContent>現時点ではモバイル版では表示が崩れます。お手数ですが、当面はスマホからのご利用の際もPC版で表示するようお願いいたします。</CardContent>
     </Card>
