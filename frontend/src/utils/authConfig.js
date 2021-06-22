e5b71e0caab81ba5134c4ccdfe6fb6d62a67401b
fujikawahiroaki <fhir0aki3@gmail.com>
2021-06-22 12:06:28 +0900

    issuer設定変更

--- frontend/src/utils/authConfig.js
+++ frontend/src/utils/authConfig.js
@@ -3,4 +3,5 @@ export default {
     clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
     redirectURI: process.env.REACT_APP_AUTH0_REDIRECT_URI,
     audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+    issuer: process.env.REACT_APP_AUTH0_ISSUER
 }
